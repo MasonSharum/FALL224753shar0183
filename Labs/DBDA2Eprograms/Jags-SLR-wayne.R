0d9cbaa9909199023e4c871801c044b65054ccee
MasonSharum <MasonSharum@gmail.com>
2025-09-29 00:29:39 -0500

    Stuff

--- Labs/DBDA2Eprograms/Jags-SLR-wayne.R
+++ Labs/DBDA2Eprograms/Jags-SLR-wayne.R
@@ -5,7 +5,7 @@
 
 # Optional generic preliminaries:
 graphics.off() # This closes all of R's graphics windows.
-rm(list=ls())  # Careful! This clears all of R's memory!
+#rm(list=ls())  # Careful! This clears all of R's memory!
 
 # Load the functions used below:
 source("DBDA2E-utilities.R") # Must be in R's current working directory.
